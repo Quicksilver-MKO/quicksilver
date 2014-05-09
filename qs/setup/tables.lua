0604666c27e58a476e6c184b3e8e1859436aaa1f
SigmaG <sigma_github@melix.net>
2014-05-09 15:35:36 +0200

    Improves how errors are treated and arguments passed

--- qs/setup/tables.lua
+++ qs/setup/tables.lua
@@ -2,21 +2,29 @@
 
 qsb = {}
 
+local err_handler = function(e)
+ local fs = package.config:sub(1,1)
+ qsv.err_msg = e:match("profiles"..fs..".*"..fs.."qs"..fs..".*lua.*")
+end
+
 -- GMCP table
 qsb.gmcp = {}
 local gmcp_metatable = {
  __index = function (t,k)
   if qsb.gmcp[k] then
-   qsf.debug("Called function qsg."..k.."()",100)
+   qss.debug("Called function qsg."..k.."()",100)
    return function (...)
-    local b,e = pcall(qsb.gmcp[k], arg)
+    local arg = ...
+    local b,e = xpcall(function () return qsb.gmcp[k](arg) end, err_handler)
     if not b then
-     qsf.error("Error in function qsg."..k..": "..e:match("profiles/%w+/.*/qs/.*lua.*"))
+     qss.error("qsg."..k.."()")
     end
    end
   else
+   qsv.err_msg = "Function does not exist"
+   qss.error("GMCP function qsg."..k.."()")
    return function (...)
-    qsf.error("Error: function qsg."..k.." does not exist")
+    return nil
    end
   end
  end,
@@ -24,8 +32,64 @@ local gmcp_metatable = {
   if type(v) == "function" then
    qsb.gmcp[k] = v
   else
-   qsf.error("Error, trying to add something to qsg that is not a function : "..k)
+   error("Attempt to add "..k.." to qsg, instead of a function")
   end
  end,
 }
 qsg = setmetatable(qsg,gmcp_metatable)
+
+-- triggers table
+qsb.triggers = {}
+local triggers_metatable = {
+ __index = function (t,k)
+  if qsb.triggers[k] then
+   qss.debug("Called function qst."..k.."()",100)
+   return function (...)
+    local arg = ...
+    local b,e = xpcall(function () return qsb.triggers[k](arg) end, err_handler)
+    if not b then
+     qss.error("qst."..k)
+    end
+   end
+  else
+   error("Attempt to access qst."..k.." which does not exist")
+  end
+ end,
+ __newindex = function (t,k,v)
+  if type(v) == "function" then
+   qsb.triggers[k] = v
+  else
+   error("Attempt to add "..k.." to qst, instead of a function")
+  end
+ end,
+}
+qst = setmetatable(qst,triggers_metatable)
+
+--functions table
+qsb.functions = {}
+local functions_metatable = {
+ __index = function (t,k)
+  if qsb.functions[k] then
+   qss.debug("Called function qsf."..k.."()",100)
+   return function (...)
+    local arg = ...
+    local b,e = xpcall(function () 
+     return qsb.functions[k](arg)
+    end, err_handler)
+    if not b then
+     qss.error("qsf."..k)
+    end
+   end
+  else
+   error("Attempt to access qsf."..k.." which does not exist")
+  end
+ end,
+ __newindex = function (t,k,v)
+  if type(v) == "function" then
+   qsb.functions[k] = v
+  else
+   error("Attempt to add "..k.." to qsf, instead of a function.")
+  end
+ end,
+}
+qsf = setmetatable(qsf,functions_metatable)
--- qs/utils/output.lua
+++ qs/utils/output.lua
@@ -1,9 +1,13 @@
 -- QS : handle the various output types
 
-function qsf.error(e)
- cecho("<red>"..e.."\n")
+function qss.error(location)
+ if not qsv.err_msg then
+  qsv.err_msg = "Unknown error"
+ end
+ tempTimer(0,string.format([[cecho("<red>ERROR in %s: <cyan>%s\n")]], location, qsv.err_msg))
+ qsv.err_msg = nil
 end
 
-function qsf.debug(m,l)
- cecho("<yellow>DEBUG"..l.." -<white> "..m.."\n")
+function qss.debug(m,l)
+ tempTimer(0,string.format([[cecho("<yellow>DEBUG%s -<white> %s\n")]],l,m))
 end
